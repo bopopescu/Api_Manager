51139d2cdf902c524617386e0bd1d419fdad8634
Dang Kai <1370465454@qq.com>
2019-04-28 17:38:05 +0800

    templates的使用

--- Api_Manager/views/views.py
+++ Api_Manager/views/views.py
@@ -4,16 +4,10 @@
 # @Date:   2019-04-28 17:19:27
 # @Last Modified time: 2019-04-28 17:19:27
 from Api_Manager import server
+from flask import render_template
 
 @server.route('/')
 @server.route("/index")
 def index():
     user = {'username':'Andy'}
-    return '''<html>
-  <head>
-    <title>Home Page</title>
-  </head>
-  <body>
-    <h1>Hello, ''' + user['username'] + '''</h1>
-  </body>
-</html>'''
+    return render_template("/main/index.html",title="主页",user=user)
